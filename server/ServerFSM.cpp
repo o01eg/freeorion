269f6fffcc595c61246b5ece077b98f178f9540d
O01eg <o01eg@yandex.ru>
2018-05-20 21:20:45 +0300

    Allow to concede with any (over 9999) colonies.

--- server/ServerFSM.cpp
+++ server/ServerFSM.cpp
@@ -559,6 +559,8 @@ MPLobby::MPLobby(my_context c) :
     m_lobby_data->m_game_rules.clear();
     m_lobby_data->m_game_rules.push_back(std::make_pair("RULE_ENABLE_SUPER_TESTER", "0"));
     m_lobby_data->m_game_rules.push_back(std::make_pair("RULE_THRESHOLD_HUMAN_PLAYER_WIN", "1"));
+    m_lobby_data->m_game_rules.push_back(std::make_pair("RULE_ALLOW_CONCEDE", "1"));
+    m_lobby_data->m_game_rules.push_back(std::make_pair("RULE_CONCEDE_COLONIES_THRESHOLD", "9999"));
 
     if (server.IsHostless()) {
         DebugLogger(FSM) << "(ServerFSM) MPLobby. Fill MPLobby data from the previous game.";

4a06c7323432f628aa5dfa1dd7c261e9ed223f7d
O01eg <o01eg@yandex.ru>
2019-05-13 00:04:16 +0300

    Load players for quickstart

--- default/python/auth/auth.py
+++ default/python/auth/auth.py
@@ -32,7 +32,7 @@ class AuthProvider:
             'p': fo.roleType.clientTypePlayer, 'o': fo.roleType.clientTypeObserver,
             'g': fo.roleType.galaxySetup
         }
-        self.default_roles = [fo.roleType.galaxySetup, fo.roleType.clientTypePlayer]
+        self.default_roles = [fo.roleType.clientTypePlayer]
         info("Auth initialized")
 
     def __parse_roles(self, roles_str):
@@ -69,9 +69,7 @@ class AuthProvider:
             error("Cann't check player %s: %s %s" % (player_name, exctype, value))
             known_login = True
 
-        if not known_login:
-            # default list of roles
-            return self.default_roles
+        # in longturn games always require authorization
         return True
 
     def is_success_auth_and_return_roles(self, player_name, auth):
@@ -96,5 +94,25 @@ class AuthProvider:
             return False
 
     def list_players(self):
-        """Not supported for public server"""
-        raise NameError('Not supported')
+        """Returns list of PlayerSetupData to use in quickstart"""
+        players = []
+        try:
+            with self.conn:
+                with self.conn.cursor() as curs:
+                    curs.execute(""" SELECT u.player_name
+                            FROM auth.users u
+                            INNER JOIN auth.contacts c
+                            ON c.player_name = u.player_name
+                            AND c.is_active
+                            AND c.delete_ts IS NULL """)
+                    for r in curs:
+                        psd = fo.PlayerSetupData()
+                        psd.player_name = r[0]
+                        psd.empire_name = r[0]
+                        psd.starting_species = "RANDOM"
+                        players.append(psd)
+        except psycopg2.InterfaceError:
+            self.conn = psycopg2.connect(self.dsn)
+            exctype, value = sys.exc_info()[:2]
+            error("Cann't load players: %s %s" % (exctype, value))
+        return players

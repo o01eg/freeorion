4ce495f9acd1f26616439e8b22ca3a7411d1661f
O01eg <o01eg@yandex.ru>
2020-05-28 20:40:42 +0300

    Add option to save game after player issued orders

--- server/ServerFSM.cpp
+++ server/ServerFSM.cpp
@@ -3296,6 +3296,12 @@ sc::result WaitingForTurnEnd::react(const CheckTurnEndConditions& c) {
         }
     }
 
+    // save game so orders from the player will be backuped
+    if (server.IsHostless() && GetOptionsDB().Get<bool>("save.auto.hostless.each-player.enabled")) {
+        PlayerConnectionPtr dummy_connection = nullptr;
+        post_event(SaveGameRequest(HostSaveGameInitiateMessage(GetAutoSaveFileName(server.CurrentTurn())), dummy_connection));
+    }
+
     return discard_event();
 }
 

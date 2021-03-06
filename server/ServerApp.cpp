4c1fa8e1b3ca2ab1bd77294df7d2a24faa9e4269
O01eg <o01eg@yandex.ru>
2021-03-06 22:59:28 +0300

    Allow AI to delegate player
    Allows locally launched AI to skip authentication.
    Allows player or AI to delegate someone without lobby if they have
    single empire to play.

--- server/ServerApp.cpp
+++ server/ServerApp.cpp
@@ -1859,8 +1859,6 @@ int ServerApp::AddPlayerIntoGame(const PlayerConnectionPtr& player_connection, i
     int empire_id = ALL_EMPIRES;
     std::list<std::string> delegation = GetPlayerDelegation(player_connection->PlayerName());
     if (target_empire_id == ALL_EMPIRES) {
-        if (!delegation.empty())
-            return ALL_EMPIRES;
         // search empire by player name
         for (auto e : Empires()) {
             if (e.second->PlayerName() == player_connection->PlayerName()) {
@@ -1868,6 +1866,20 @@ int ServerApp::AddPlayerIntoGame(const PlayerConnectionPtr& player_connection, i
                 empire = e.second;
                 break;
             }
+        }
+        // Assign player to empire if he doesn't have own empire and delegates signle
+        if (delegation.size() == 1 && empire == nullptr) {
+            for (auto e : Empires()) {
+                if (e.second->PlayerName() == delegation.front()) {
+                    empire_id = e.first;
+                    empire = e.second;
+                    break;
+                }
+            }
+        }
+        if (!delegation.empty()) {
+            DebugLogger() << "ServerApp::AddPlayerIntoGame(...): Player should choose between delegates.";
+            return ALL_EMPIRES;
         }
     } else {
         // use provided empire and test if it's player himself or one of delegated

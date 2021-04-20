d598ee0e11b7537f24e411647f2242395acb6e09
O01eg <o01eg@yandex.ru>
2021-04-20 21:34:53 +0300

    Start to try to deploy from Github Actions CI

--- .github/workflows/ubuntu-20.04-install-build.sh
+++ .github/workflows/ubuntu-20.04-install-build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
 sudo apt update
-sudo apt install -y libboost1.71-all-dev \
-	libglew-dev \
-	libogg-dev \
-	libopenal-dev \
-	libsdl2-dev \
-	libvorbis-dev \
-	cppcheck \
-	doxygen \
-	python3-pip \
-	ccache \
-	clang \
-	godot3-server
-
+sudo apt install -y devscripts \
+        dput \
+        python3-paramiko \
+        fakeroot \
+        cmake \
+        debhelper \
+        libalut-dev \
+        libboost-all-dev \
+        libgl1-mesa-dev \
+        libglew-dev \
+        libogg-dev \
+        libopenal-dev \
+        libsdl2-dev \
+        libvorbis-dev

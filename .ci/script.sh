b1ccd992c637c84c77ed311aff08fbcece3a7bb6
O01eg <o01eg@yandex.ru>
2021-10-19 20:32:18 +0000

    Ignore MSVC2022

--- .ci/script.sh
+++ .ci/script.sh
@@ -16,6 +16,6 @@ DIST=$1
 DISTNAME=../freeorion_0.4.10.999+1SNAPSHOT${BUILD_DATE}ppa${BUILD_PPA}~${DIST}
 echo "Building package ${DISTNAME}..."
 sed -i "1,+1{s/bionic\|disco\|eoan\|focal\|groovy\|hirsute\|impish\|jammy/${DIST}/g}" debian/changelog  || end_with_error "Cann't set distribution"
-XZ_OPT=-9 tar --xz -cf ${DISTNAME}.orig.tar.xz --exclude .git --exclude .ci --exclude .github --exclude msvc2017 --exclude msvc2019 .  || end_with_error "Cann't make archive"
+XZ_OPT=-9 tar --xz -cf ${DISTNAME}.orig.tar.xz --exclude .git --exclude .ci --exclude .github --exclude msvc2017 --exclude msvc2019 --exclude msvc2022 .  || end_with_error "Cann't make archive"
 debuild -S -sa || end_with_error "Cann't make package"
 

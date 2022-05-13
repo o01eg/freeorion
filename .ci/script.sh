cba7c8409f2ed6885cc7d05343e8206891d7b10c
O01eg <o01eg@yandex.ru>
2022-05-13 22:16:30 +0300

    Add support for Ubuntu 22.10 Kinetic Kudu

--- .ci/script.sh
+++ .ci/script.sh
@@ -8,7 +8,7 @@ sed -i "s/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\.[0-9a-f]\{7\}\]/${BUILD_REV}\]/" uti
 head -n 1 debian/changelog
 
 echo "Building package ${DISTNAME}..."
-sed -i "1,+1{s/bionic\|disco\|eoan\|focal\|groovy\|hirsute\|impish\|jammy/${DIST}/g}" debian/changelog  || end_with_error "Cann't set distribution"
+sed -i "1,+1{s/bionic\|disco\|eoan\|focal\|groovy\|hirsute\|impish\|jammy\|kinetic/${DIST}/g}" debian/changelog  || end_with_error "Cann't set distribution"
 sed -i "1,+1{s/1SNAPSHOT[0-9]\{8\}ppa[0-9]\+~/1SNAPSHOT${BUILD_DATE}ppa${BUILD_PPA}~/g}" debian/changelog || end_with_error "Cann't set date"
 
 head -n 1 debian/changelog

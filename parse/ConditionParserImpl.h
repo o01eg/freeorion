694e39f522048d4f4ecffc361164486b75c470d8
O01eg <o01eg@yandex.ru>
2023-02-24 10:00:31 +0300

    Remove more old parser files

--- parse/ConditionParserImpl.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ConditionParserImpl_h_
-#define _ConditionParserImpl_h_
-
-#include "ConditionParser.h"
-
-#define DEBUG_CONDITION_PARSERS 0
-
-#endif
--- parse/EffectParserImpl.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EffectParserImpl_h_
-#define _EffectParserImpl_h_
-
-#include "EffectParser.h"
-
-#define DEBUG_EFFECT_PARSERS 0
-
-#endif

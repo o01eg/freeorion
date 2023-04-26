55c2019a72ad38c1af1b1028ba441a15af20e68c
geoffthemedio <geoffthemedio@gmail.com>
2023-04-26 13:05:36 +0200

    grooming

--- server/ServerApp.cpp
+++ server/ServerApp.cpp
@@ -167,13 +167,14 @@ void ServerApp::SignalHandler(const boost::system::error_code& error, int signal
 }
 
 namespace {
-    std::string AIClientExe()
-    {
+    std::string AIClientExe() {
+        static constexpr std::string_view ai_client_exe_filename =
 #ifdef FREEORION_WIN32
-        return (GetBinDir() / "freeorionca.exe").string();
+            "freeorionca.exe";
 #else
-        return (GetBinDir() / "freeorionca").string();
+            "freeorionca";
 #endif
+        return (GetBinDir() / ai_client_exe_filename).string();
     }
 }
 

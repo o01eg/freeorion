28399d7649fec32b160c1328a6f1e95daaafad7f
O01eg <o01eg@yandex.ru>
2024-11-20 07:45:16 +0400

    Switch to common keys for PPA deploy

--- .github/pre-deploy.sh
+++ .github/pre-deploy.sh
@@ -6,12 +6,13 @@ chmod 600 .github/id_ed25519 || exit 1
 { echo "${DEPLOY_SSH_PASSWORD}" | gpg --batch --yes --skip-verify --passphrase-fd 0 --output .github/secring.auto -d .github/secring.auto.gpg ; } || exit 1
 chmod 600 .github/secring.auto || exit 1
 
-gpg --import .github/secring.auto
-gpg --import .github/pubring.auto
+{ gpg --import .github/secring.auto ; } || { end_with_error "Could not import secret keyring into gpg." ; }
+{ gpg --import .github/pubring.auto ; } || { end_with_error "Could not import public keyring into gpg." ; }
 
 mkdir -p $HOME/.ssh/
 chmod 700 $HOME/.ssh/
 ssh-keyscan frs.sourceforge.net >> $HOME/.ssh/known_hosts
+ssh-keyscan ppa.launchpad.net >> $HOME/.ssh/known_hosts
 echo "StrictHostKeyChecking no" >> $HOME/.ssh/config
 echo "IdentityFile $(pwd)/.github/id_ed25519" >> $HOME/.ssh/config
 echo "PubkeyAuthentication yes" >> $HOME/.ssh/config

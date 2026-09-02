3a817ebdfc7a06ccf5046f32e520a6ba43ae6cdf
O01eg <397177+o01eg@users.noreply.github.com>
2026-09-02 19:56:10 +0400

    [..] Fix output export

--- godot4/addons/android-export/android_export.gd
+++ godot4/addons/android-export/android_export.gd
@@ -3,19 +3,27 @@ extends EditorPlugin
 
 var export_plugin: AndroidExportPlugin
 
-class AndroidExportPlugin extends EditorExportPlugin:
-    func _get_name() -> String:
-        return "AndroidExportPlugin"
 
-    func _get_android_manifest_activity_element_contents(platform: EditorExportPlatform, debug: bool) -> String:
-        return '''
+class AndroidExportPlugin:
+	extends EditorExportPlugin
+
+	func _get_name() -> String:
+		return "AndroidExportPlugin"
+
+	func _get_android_manifest_activity_element_contents(
+		platform: EditorExportPlatform, debug: bool
+	) -> String:
+		print("FreeOrion AndroidExportPlugin: patched activiy element")
+		return '''
         <meta-data android:name="android.app.shortcuts" android:resource="@xml/shortcuts"/>
         '''
 
+
 func _enter_tree() -> void:
-    export_plugin = AndroidExportPlugin.new()
-    add_export_plugin(export_plugin)
+	export_plugin = AndroidExportPlugin.new()
+	add_export_plugin(export_plugin)
+
 
 func _exit_tree() -> void:
-    remove_export_plugin(export_plugin)
-    export_plugin = null
+	remove_export_plugin(export_plugin)
+	export_plugin = null

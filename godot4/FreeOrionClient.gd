22515a3282636f736c3b4abb049f3db135238edc
O01eg <397177+o01eg@users.noreply.github.com>
2026-09-09 22:18:35 +0400

    [..] Godot4 Android plugin for server

--- godot4/FreeOrionClient.gd
+++ godot4/FreeOrionClient.gd
@@ -4,3 +4,10 @@ extends Control
 func _ready():
 	GlobalFreeOrionNode.start_network_thread()
 	GlobalFreeOrionNode.start_parsing_thread()
+
+	GlobalFreeOrionNode.parsing_completed.connect(_on_freeorion_parsing_completed)
+
+
+func _on_freeorion_parsing_completed():
+	if GlobalFreeOrionNode.options_get_bool("quickstart"):
+		GlobalFreeOrionNode.new_single_player_game()

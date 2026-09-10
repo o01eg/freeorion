62f1a88cbac7b205943e84c051f851ae2e0eba3a
O01eg <397177+o01eg@users.noreply.github.com>
2026-09-10 11:10:49 +0400

    [..] Add player_name parameter to AndroidAIService constructor

--- server/AndroidAIService.h
+++ server/AndroidAIService.h
@@ -1,9 +1,11 @@
 #ifndef _AndroidAIService_h_
 #define _AndroidAIService_h_
 
+#include <string>
+
 class AndroidAIService {
 public:
-    AndroidAIService(int slot_id);
+    AndroidAIService(int slot_id, const std::string& player_name);
     ~AndroidAIService();
 
     AndroidAIService(const AndroidAIService&) = delete;
@@ -11,8 +13,11 @@ public:
 
     AndroidAIService(AndroidAIService&&) noexcept = default;
     AndroidAIService& operator=(AndroidAIService&&) noexcept = default;
+
+    void Kill();
 private:
     int m_slot_id;
+    bool m_killed;
 };
 
 #endif

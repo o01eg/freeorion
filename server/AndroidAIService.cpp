1e888722eb646d05ee16611db15dd5e44cf6a455
O01eg <397177+o01eg@users.noreply.github.com>
2026-09-10 11:11:39 +0400

    [..] Enhance AndroidAIService with player_name parameter

--- server/AndroidAIService.cpp
+++ server/AndroidAIService.cpp
@@ -3,15 +3,24 @@
 #include "../util/AndroidEnvironment.h"
 
 namespace {
-    void ControlAndroidService(std::string class_name, bool start) {
+    void ControlAndroidService(const std::string& class_name, bool start, const std::string& player_name = "") {
         ScopedJNIEnv env;
         jobject context = env->NewLocalRef(ScopedJNIEnv::Context());
         jclass service_cls = env->FindClass(class_name.c_str());
         jclass intent_cls = env->FindClass("android/content/Intent");
         jmethodID intent_ctor_mid = env->GetMethodID(intent_cls, "<init>", "(Landroid/content/Context;Ljava/lang/Class;)V");
         jobject intent = env->NewObject(intent_cls, intent_ctor_mid, context, service_cls);
-        jclass context_cls = env->GetObjectClass(context);
 
+        if (!player_name.empty()) {
+            jmethodID put_extra_mid = env->GetMethodID(intent_cls, "putExtra", "(Ljava/lang/String;Ljava/lang/String;)Landroid/content/Intent;");
+            jstring key = env->NewStringUTF("player_name");
+            jstring value = env->NewStringUTF(player_name.c_str());
+            env->CallObjectMethod(intent, put_extra_mid, key, value);
+            env->DeleteLocalRef(key);
+            env->DeleteLocalRef(value);
+        }
+
+        jclass context_cls = env->GetObjectClass(context);
         if (start) {
             jmethodID start_service_mid = env->GetMethodID(context_cls, "startService", "(Landroid/content/Intent;)Landroid/content/ComponentName;");
             env->CallObjectMethod(context, start_service_mid, intent);
@@ -24,9 +33,16 @@ namespace {
     }
 }
 
-AndroidAIService::AndroidAIService(int slot_id)
+AndroidAIService::AndroidAIService(int slot_id, const std::string& player_name)
     : m_slot_id(slot_id)
-{ ControlAndroidService("org/freeorion/godot/FreeOrionAIService" + std::to_string(m_slot_id), true); }
+{ ControlAndroidService("org/freeorion/godot/FreeOrionAIService" + std::to_string(m_slot_id), true, player_name); }
 
 AndroidAIService::~AndroidAIService()
-{ ControlAndroidService("org/freeorion/godot/FreeOrionAIService" + std::to_string(m_slot_id), false); }
+{ Kill(); }
+
+void AndroidAIService::Kill() {
+    if (!m_killed) {
+        ControlAndroidService("org/freeorion/godot/FreeOrionAIService" + std::to_string(m_slot_id), false);
+        m_killed = true;
+    }
+}

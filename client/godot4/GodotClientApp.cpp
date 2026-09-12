3bc0aa288756e2fc74ee1c8bffd376baaec9b999
O01eg <397177+o01eg@users.noreply.github.com>
2026-09-12 09:54:41 +0400

    [..] Reduce effects to fix thread pool issue

--- client/godot4/GodotClientApp.cpp
+++ client/godot4/GodotClientApp.cpp
@@ -113,6 +113,8 @@ void GodotClientApp::StartServer() {
                 args.append("--singleplayer");
                 args.append("--skip-checksum");
             }
+            args.append("--effects.server.threads");
+            args.append("1");
             plugin->call("startServer", args);
         } else
             ErrorLogger() << "GodotClientApp::StartServer: FreeOrion plugin singleton not found";
